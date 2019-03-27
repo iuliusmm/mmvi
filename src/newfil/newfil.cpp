d15b6fcdf9014ebda4b374a4766044e0b25fdc03
iuliusmm <iuliusmm@outlook.com>
2019-03-27 14:00:53 +0100

    Uses exit_code_t in newfil

--- src/newfil/newfil.cpp
+++ src/newfil/newfil.cpp
@@ -5,15 +5,14 @@
 #include<cstring>
 #include<fstream>
 #include<filesystem>
-#include<experimental/filesystem>
 
 #include<mmvi.hpp>
 #include<argtable3.h>
 #include<fmt/format.h>
 #include<fmt/ostream.h>
 
-int create_file(int overwrite, int verbose, const char **file, int nfile) {
-	int exitcode = EXIT_SUCCESS;
+exit_code_t create_file(int overwrite, int verbose, const char **file, int nfile) {
+	exit_code_t exit_code = EXIT_SUCCESS;
 	std::ofstream f;
 	for (int i = 0; i < nfile; ++i) {
 		if (overwrite > 0) {
@@ -21,23 +20,23 @@ int create_file(int overwrite, int verbose, const char **file, int nfile) {
 			f << "";
 			if (!f.good()) {
 				fmt::print(stderr, "{}: cannot create file '{}': {}\n", program_name, file[i], strerror(errno));
-				exitcode = EXIT_FAILURE;
+				exit_code = EXIT_FAILURE;
 				continue;
 			}
 			f.close();
 
 		}
 		else {
-			if (std::experimental::filesystem::exists(file[i])) {
+			if (std::filesystem::exists(file[i])) {
 				fmt::print(stderr, "{}: cannot create file '{}': File exists\n", program_name, file[i]);
-				exitcode = EXIT_FAILURE;
+				exit_code = EXIT_FAILURE;
 				continue;
 			}
 			f.open(file[i], std::ios::out);
 			f << "";
 			if (!f.good()) {
 				fmt::print(stderr, "{}: cannot create file '{}': {}\n", program_name, file[i], strerror(errno));
-				exitcode = EXIT_FAILURE;
+				exit_code = EXIT_FAILURE;
 				continue;
 			}
 			f.close();
@@ -46,7 +45,7 @@ int create_file(int overwrite, int verbose, const char **file, int nfile) {
 			fmt::print("{}: created file '{}'\n", program_name, file[i]);
 		}
 	}
-	return exitcode;
+	return exit_code;
 }
 
 int main(int argc, char **argv) {
@@ -57,15 +56,15 @@ int main(int argc, char **argv) {
 	struct arg_file *file = arg_filen(NULL, NULL, "FILE", 1, 1000, NULL);
 	struct arg_end  *end = arg_end(20);
 	void *argtable[] = { overwrite, verbose, help, version, file, end };
-	int exitcode = EXIT_SUCCESS;
 	int nerrors;
 
 	// Verify the argtable[] entries were allocated sucessfully
 	if (arg_nullcheck(argtable) != 0) {
 		// NULL entries were detected, some allocations must have failed
 		fmt::print("{}: insufficient memory\n", program_name);
-		exitcode = EXIT_FAILURE;
-		goto exit;
+		//deallocate each non-null entry in argtable[]
+		arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+		return EXIT_FAILURE;
 	}
 
 	// Parse the command line as defined by argtable[]
@@ -78,17 +77,18 @@ int main(int argc, char **argv) {
 		fmt::print("Create the FILE(s).\n\n");
 		arg_print_glossary(stdout, argtable, "  %-20s %s\n");
 		fmt::print("\nCopyright (C) 2019 Julius Behrens. All rights reserved.\n");
-
-		exitcode = EXIT_SUCCESS;
-		goto exit;
+		//deallocate each non-null entry in argtable[]
+		arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+		return EXIT_SUCCESS;
 	}
 
 	// special case: '--version' takes precedence error reporting
 	if (version->count > 0) {
 		fmt::print("{} {}.{}\n", program_name, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR);
 		fmt::print("Copyright (C) 2019 Julius Behrens. All rights reserved.\n");
-		exitcode = EXIT_SUCCESS;
-		goto exit;
+		//deallocate each non-null entry in argtable[]
+		arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+		return EXIT_SUCCESS;
 	}
 
 	// If the parser returned any errors then display them and exit
@@ -96,14 +96,14 @@ int main(int argc, char **argv) {
 		// Display the error details contained in the arg_end struct.
 		arg_print_errors(stderr, end, program_name.c_str());
 		fmt::print(stderr, "Try '{} --help' for more information.\n", program_name);
-		exitcode = EXIT_FAILURE;
-		goto exit;
+		//deallocate each non-null entry in argtable[]
+		arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+		return EXIT_FAILURE;
 	}
 
-	exitcode = create_file(overwrite->count, verbose->count, file->filename, file->count);
+	exit_code_t exit_code = create_file(overwrite->count, verbose->count, file->filename, file->count);
 
-	exit:
 	//deallocate each non-null entry in argtable[]
 	arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
-	return exitcode;
+	return exit_code;
 }
